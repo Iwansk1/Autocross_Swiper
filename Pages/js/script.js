b768591189b4e1adb43d2a21559a7ea34e392626
Iwan Bijl <Iwanbijl@gmail.com>
2024-06-24 20:52:11 +0200

    back to top button

--- Pages/js/script.js
+++ Pages/js/script.js
@@ -101,6 +101,26 @@ document.getElementById("more-info").addEventListener("click", function () {
 });
 
 
+// Back to top button
+var mybutton = document.getElementsByClassName("top-btn")[0];
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 //Experiments 
 
 // // Function to update the car information display based on the active slide
